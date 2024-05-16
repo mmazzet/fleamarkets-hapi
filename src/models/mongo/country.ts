4f6cdde67b7276bbee905605519fa2befcb47a3b
Marianna Mazzette <20104120@mail.wit.ie>
2024-05-16 16:48:31 +0100

    before adding charts

--- src/models/mongo/country.ts
+++ src/models/mongo/country.ts
@@ -4,7 +4,6 @@ import { Country } from "../../types/fleamarket-types";
 const countrySchema = new Schema<Country>({
   firstName: String,
   lastName: String,
-  office: String,
 });
 
 export const CountryMongoose = model("Country", countrySchema);
--- src/types/fleamarket-types.ts
+++ src/types/fleamarket-types.ts
@@ -9,7 +9,6 @@ export interface User {
 export interface Country {
   firstName: string;
   lastName: string;
-  office: string;
   _id: string;
 }
 
